7a0696d0109b794480995547abd65b8635ce20ab
Veronica Williams <vcwilliams2014@gmail.com>
2017-10-30 18:23:25 -0700

    Updating Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,4 @@
 
-// $(document).ready(function() {
-
-// array of number options for computer
-var number=["10", "12", "25", "36", "15", "200", "45"];
-// crystal number will be held here.
-var displayNumber = "";
-
-// array of random numbers for crystals:
-var cone = ["1", "2", "3"];
-var displayCone = "";
-
-var ctwo = ["2", "4", "6",];
-var displayCtwo = "";
-
-var cthree = ["3", "6", "9"];
-var displayCthree = "";
-
-var cfour = ["10", "11", "5"];
-var displayCfour = "";
-// array for crystals ends here.
-
-// the rand variable is working and giving me the numbers from the array.
-displayNumber = number[Math.floor(Math.random()* number.length)];
-console.log(displayNumber); 
-// I need to write the above code in my desired div
-
-// I need to assign these random numbers to the crystals.
-displayCone = cone[Math.floor(Math.random()* cone.length)];
-console.log(displayCone); 
-
-displayCtwo = ctwo[Math.floor(Math.random()* ctwo.length)];
-console.log(displayCtwo); 
-
-displayCthree = cthree[Math.floor(Math.random()* cthree.length)];
-console.log(displayCthree); 
-
-displayCfour = cfour[Math.floor(Math.random()* cfour.length)];
-console.log(displayCfour); 
-// random numbers for crystals end here.
-
-
-
-
-
-// var ytally = [];
-// // var winResults = alert("YOU WIN!!!");
-// // var loseResults = alert("BORN LOSER!!");
-// var win = 0;
-// var losses =0;
 
 
 
@@ -62,10 +13,4 @@ console.log(displayCfour);
 
 
 
-
-
-
-
-// var win = 0;
-// var losses = 0;
 
