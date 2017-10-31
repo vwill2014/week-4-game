fe4af1d4f0c67a3d12999379917ebf0c54a72dc6
Veronica Williams <vcwilliams2014@gmail.com>
2017-10-30 19:05:18 -0700

    Updating Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,25 @@
 
+var targetNumber = 50;
+$("#crystalNumber").html(targetNumber);
 
+
+var counter = 0
+
+$(".image").on("click", function(){
+
+counter +=10;
+
+$("#userTally").html(counter);
+
+
+if(counter===targetNumber){
+
+alert("You win!!!");
+
+
+}
+
+});
 
 
 
